22a9a8063b96413cb862dd9c795c483911d75edc
LeTran30 <let9888@gmail.com>
2025-06-19 16:41:33 +0700

    Update video

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,19 @@
 import { Container } from "react-bootstrap";
 import { fetchVideoLink } from "../../data";
 import "./Hero.css";
-
+import ReactPlayer from "react-player";
 export const Hero = () => {
   const videoLink = fetchVideoLink();
 
   return (
     <section id="hero">
       <Container className="video-container">
-        <video controls autoPlay>
-          <source src="images/videoplayback.mp4" type="video/mp4" />
-        </video>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=HZN-lthHJN4&list=RDHZN-lthHJN4&start_radio=1"
+          controls
+          width="100%"
+          height="100%"
+        />
       </Container>
     </section>
   );
